3cc12607d8ae091653a1042a14d5b7c965b0e865
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-06-21 15:47:37 -0500

    comments edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+
+
+    def index 
+        @comments = Comment.all 
+    end
+
+    def show 
+        @comment = Comment.find(params[:id])
+    end
+
+    def new 
+        @comment = Comment.new 
+    end
+
+    def create 
+        @comment = Comment.create(comment_params)
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    
 end
