52f289c2334ed1f980c5c0db1cf457bbd1bcf74c
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-06-23 09:51:48 -0500

    post

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,39 @@
 class FavoritesController < ApplicationController
+
+    def index 
+        @favorites = Favorite.all
+      end
+  
+      def show 
+        @favorite = Favorite.find(params[:id])
+      end
+  
+      def new 
+        @favorite = Favorite.new
+      end
+  
+      def create 
+        @favorite = Favorite.create(favorite_params)
+      end
+  
+      def edit 
+          @favorite = Favorite.find(params[:id])
+      end
+  
+      def update 
+         @favorite = Favorite.find(params[:id])
+         @favorite.update(favorite_params)
+      end
+  
+      def destroy 
+        @favorite = Favorite.find(params[:id])
+        @favorite.destroy
+      end
+  
+      private 
+  
+      def favorite_params
+          params.permit(:favorite).permit(:)
+      end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+    def index 
+        @posts = Posts.all
+      end
+  
+      def show 
+        @post = Post.find(params[:id])
+      end
+  
+      def new 
+        @post = Post.new
+      end
+  
+      def create 
+        @post = Post.create(post_params)
+      end
+  
+      def edit 
+          @post = Post.find(params[:id])
+      end
+  
+      def update 
+         @post = Post.find(params[:id])
+         @post.update(post_params)
+      end
+  
+      def destroy 
+        @post = Post.find(params[:id])
+        @post.destroy
+      end
+  
+      private 
+  
+      def post_params
+          params.permit(:post).permit(:title, :body)
+      end
 end
