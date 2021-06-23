513f8b0d4741c011b1178e2815a8a7cd04a57775
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-06-23 09:59:06 -0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+    def index 
+        @user = User.all
+      end
+  
+      def show 
+        @user = User.find(params[:id])
+        @activities = Activity.all
+      end
+  
+      def new 
+        @user = User.new
+      end
+  
+      def create 
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+      end
+  
+      def edit 
+          @user = User.find(params[:id])
+      end
+  
+      def update 
+         @user = User.find(params[:id])
+         @user.update(user_params)
+      end
+  
+      def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+      end
+  
+      private 
+  
+      def user_params
+          params.require(:user).permit(:username, :email, :password)
+      end
 end
