377dc209100761026c7b2267cb97a14d84f5467c
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-07-11 17:12:33 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 u1 = User.create(username: "U1", email: "u1@email.com", password: "abc123")
 u2 = User.create(username: "U2", email: "u2@email.com", password: "abc123")
-u3 = User.create(username: "U2", email: "u2@email.com", password: "abc123")
-u4 = User.create(username: "U2", email: "u2@email.com", password: "abc123")
+u3 = User.create(username: "U3", email: "u2@email.com", password: "abc123")
+u4 = User.create(username: "U4", email: "u2@email.com", password: "abc123")
+u5 = User.create(username: "U5", email: "u2@email.com", password: "abc123")
 
 
 a1 = Activity.create(name: "Act1", description: "Is a jolly good time!")
