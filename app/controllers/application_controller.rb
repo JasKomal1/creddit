92d88e758bcd8428ec75d14bc3b927250434b658
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-08-08 18:12:07 -0500

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-    ef index 
-        @comments = Comment.all 
-    end
-
-    def show 
-        @comment = Comment.find(params[:id])
-    end
-
-    def new 
+   
 end
